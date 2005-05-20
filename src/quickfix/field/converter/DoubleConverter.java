f82ee67eb6f9b52bf83acf501ff0243d77bfbc93
Steven Bate <steve@stevebate.net>
2005-05-20 02:50:10 +0000

    Fixed double formatter (bad parsing in non-US locales)

--- src/quickfix/field/converter/DoubleConverter.java
+++ src/quickfix/field/converter/DoubleConverter.java
@@ -1,6 +1,8 @@
 package quickfix.field.converter;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,9 +10,15 @@ import quickfix.FieldConvertError;
 
 
 public class DoubleConverter {
-    private static DecimalFormat doubleFormat = new DecimalFormat("#.##############");
+    private static DecimalFormat doubleFormat = null;
     private static final Pattern doublePattern = Pattern.compile("-?\\d*(\\.\\d*)?");
 
+    static {
+        doubleFormat = new DecimalFormat("#.##############");
+        // Assigning Locale.US is necessary to avoid mixed up with decimal separators ex : , for France and . for US 
+        doubleFormat.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.US));
+    }
+    
     public static String convert(double d) {
         // TODO This can be optimized!
         synchronized (doubleFormat) {
