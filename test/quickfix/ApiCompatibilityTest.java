91bc39a19e89e16fe8ea29903cc12a7567254a7b
Steven Bate <steve@stevebate.net>
2005-07-06 00:32:18 +0000

    Added quickfix JNI library to lib directory for compatbility testing.

--- test/quickfix/ApiCompatibilityTest.java
+++ test/quickfix/ApiCompatibilityTest.java
@@ -281,8 +281,7 @@ public class ApiCompatibilityTest {
             }
         };
         try {
-            // TODO Move copy of JNI library into QuickFIX/J repository
-            String jarPath = "../quickfix_cpp/lib/quickfix.jar";
+            String jarPath = "lib/quickfix-jni.jar";
             URL[] urls = new URL[] { new URL("file:" + jarPath) };
             ClassLoader jniClassLoader = new URLClassLoader(urls, null);
             IgnoredItems ignoredItems = new IgnoredItems(jniClassLoader);
