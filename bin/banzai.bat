9c64715e19108820478eaddca1fe356093b88699
Steven Bate <steve@stevebate.net>
2005-07-18 04:13:00 +0000

    Updated library and added version numbers

--- bin/banzai.bat
+++ bin/banzai.bat
@@ -3,8 +3,8 @@ set CLASSPATH=quickfixj.jar
 set CLASSPATH=%CLASSPATH%;output/ant/jars/quickfixj.jar
 set CLASSPATH=%CLASSPATH%;output/eclipse/classes
 set CLASSPATH=%CLASSPATH%;lib/tl-netty2-1.8.0.jar
-set CLASSPATH=%CLASSPATH%;lib/backport-util-concurrent.jar
-set CLASSPATH=%CLASSPATH%;lib/commons-logging.jar
+set CLASSPATH=%CLASSPATH%;lib/backport-util-concurrent-2.0.jar
+set CLASSPATH=%CLASSPATH%;lib/commons-logging-1.0.4.jar
 
 cd ..
 java quickfix.examples.banzai.Banzai
--- bin/banzai.sh
+++ bin/banzai.sh
@@ -4,8 +4,8 @@ CLASSPATH=quickfixj.jar
 CLASSPATH=$CLASSPATH:output/ant/jars/quickfixj.jar
 CLASSPATH=$CLASSPATH:output/eclipse/classes
 CLASSPATH=$CLASSPATH:lib/tl-netty2-1.8.0.jar
-CLASSPATH=$CLASSPATH:lib/backport-util-concurrent.jar
-CLASSPATH=$CLASSPATH:lib/commons-logging.jar
+CLASSPATH=$CLASSPATH:lib/backport-util-concurrent-2.0.jar
+CLASSPATH=$CLASSPATH:lib/commons-logging-1.0.4.jar
 
 cd ..
 java quickfix.examples.banzai.Banzai
