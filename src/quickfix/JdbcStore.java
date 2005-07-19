f7fc1694147cb2fb1f1ab715821a467657eb9ce3
Steven Bate <steve@stevebate.net>
2005-07-19 23:43:19 +0000

    Added finally blocks for result set closing, added statement caching, and refactored queries.

--- src/quickfix/JdbcStore.java
+++ src/quickfix/JdbcStore.java
@@ -1,21 +1,21 @@
 /****************************************************************************
-** Copyright (c) 2001-2005 quickfixengine.org  All rights reserved.
-**
-** This file is part of the QuickFIX FIX Engine
-**
-** This file may be distributed under the terms of the quickfixengine.org
-** license as defined by quickfixengine.org and appearing in the file
-** LICENSE included in the packaging of this file.
-**
-** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-**
-** See http://www.quickfixengine.org/LICENSE for licensing information.
-**
-** Contact ask@quickfixengine.org if any conditions of this licensing are
-** not clear to you.
-**
-****************************************************************************/
+ ** Copyright (c) 2001-2005 quickfixengine.org  All rights reserved.
+ **
+ ** This file is part of the QuickFIX FIX Engine
+ **
+ ** This file may be distributed under the terms of the quickfixengine.org
+ ** license as defined by quickfixengine.org and appearing in the file
+ ** LICENSE included in the packaging of this file.
+ **
+ ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ **
+ ** See http://www.quickfixengine.org/LICENSE for licensing information.
+ **
+ ** Contact ask@quickfixengine.org if any conditions of this licensing are
+ ** not clear to you.
+ **
+ ****************************************************************************/
 
 package quickfix;
 
@@ -29,13 +29,40 @@ import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.TimeZone;
 
 class JdbcStore implements MessageStore {
     private MemoryStore cache = new MemoryStore();
     private Connection connection;
     private SessionID sessionID;
+    private HashMap statementCache = new HashMap();
 
+    private static final String SQL_UPDATE_SEQNUMS = "UPDATE sessions SET incoming_seqnum=?, "
+            + "outgoing_seqnum=? WHERE beginstring=? and sendercompid=? "
+            + "and targetcompid=? and session_qualifier=?";
+    private static final String SQL_INSERT_SESSION = "INSERT INTO sessions (beginstring, sendercompid, "
+            + "targetcompid, session_qualifier, creation_time, "
+            + "incoming_seqnum, outgoing_seqnum) VALUES(?,?,?,?,?,?,?)";
+    private static final String SQL_GET_SEQNUMS = "SELECT creation_time, incoming_seqnum, "
+            + "outgoing_seqnum FROM sessions WHERE beginstring=? and  "
+            + "sendercompid=? and targetcompid=? and session_qualifier=?";
+    private static final String INSERT_UPDATE_MESSAGE = "UPDATE messages SET message=? "
+            + "WHERE beginstring=? and sendercompid=? "
+            + "and targetcompid=? and session_qualifier=? and msgseqnum=?";
+    private static final String SQL_INSERT_MESSAGE = "INSERT INTO messages (beginstring, "
+            + "sendercompid, targetcompid, session_qualifier, msgseqnum, "
+            + "message) VALUES (?,?,?,?,?,?)";
+    private static final String SQL_GET_MESSAGES = "SELECT message FROM messages WHERE  "
+            + "beginstring=? and sendercompid=? and targetcompid=? and "
+            + "session_qualifier=? and msgseqnum>=? and msgseqnum<=? " + "ORDER BY msgseqnum";
+    private static final String SQL_UPDATE_SESSION = "UPDATE sessions SET creation_time=?, "
+            + "incoming_seqnum=?, outgoing_seqnum=? "
+            + "WHERE beginstring=? and sendercompid=? and "
+            + "targetcompid=? and session_qualifier=?";
+    private static final String SQL_DELETE_MESSAGES = "DELETE FROM messages WHERE "
+            + "beginstring=? and sendercompid=? " + "and targetcompid=? and session_qualifier=?";
+    
     public JdbcStore(SessionSettings settings, SessionID sessionID) throws Exception {
         this.sessionID = sessionID;
         connection = connect(settings, sessionID);
@@ -48,34 +75,41 @@ class JdbcStore implements MessageStore {
     }
 
     private void loadCache() throws SQLException, IOException {
-        PreparedStatement query = connection
-                .prepareStatement("SELECT creation_time, incoming_seqnum, "
-                        + "outgoing_seqnum FROM sessions WHERE beginstring=? and  "
-                        + "sendercompid=? and targetcompid=? and session_qualifier=?");
+        PreparedStatement query = getPreparedStatement(SQL_GET_SEQNUMS);
         query.setString(1, sessionID.getBeginString());
         query.setString(2, sessionID.getSenderCompID());
         query.setString(3, sessionID.getTargetCompID());
         query.setString(4, sessionID.getSessionQualifier());
-        ResultSet rs = query.executeQuery();
-        if (rs.next()) {
-            Calendar timestamp = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-            timestamp.setTime(rs.getDate(1));
-            cache.setNextTargetMsgSeqNum(rs.getInt(2));
-            cache.setNextSenderMsgSeqNum(rs.getInt(3));
-        } else {
-            PreparedStatement insert = connection
-                    .prepareStatement("INSERT INTO sessions (beginstring, sendercompid, "
-                            + "targetcompid, session_qualifier, creation_time, "
-                            + "incoming_seqnum, outgoing_seqnum) VALUES(?,?,?,?,?,?,?)");
-            insert.setString(1, sessionID.getBeginString());
-            insert.setString(2, sessionID.getSenderCompID());
-            insert.setString(3, sessionID.getTargetCompID());
-            insert.setString(4, sessionID.getSessionQualifier());
-            insert.setTimestamp(5, new Timestamp(cache.getCreationTime().getTime()));
-            insert.setInt(6, cache.getNextTargetMsgSeqNum());
-            insert.setInt(7, cache.getNextSenderMsgSeqNum());
-            insert.execute();
+        ResultSet rs = null;
+        try {
+            rs = query.executeQuery();
+            if (rs.next()) {
+                cache.setCreationTime(SystemTime.getUtcCalendar(rs.getDate(1)));
+                cache.setNextTargetMsgSeqNum(rs.getInt(2));
+                cache.setNextSenderMsgSeqNum(rs.getInt(3));
+            } else {
+                PreparedStatement insert = getPreparedStatement(SQL_INSERT_SESSION);
+                insert.setString(1, sessionID.getBeginString());
+                insert.setString(2, sessionID.getSenderCompID());
+                insert.setString(3, sessionID.getTargetCompID());
+                insert.setString(4, sessionID.getSessionQualifier());
+                insert.setTimestamp(5, new Timestamp(cache.getCreationTime().getTime()));
+                insert.setInt(6, cache.getNextTargetMsgSeqNum());
+                insert.setInt(7, cache.getNextSenderMsgSeqNum());
+                insert.execute();
+            }
+        } finally {
+            rs.close();
         }
+    }
+
+    private PreparedStatement getPreparedStatement(String sql) throws SQLException {
+        PreparedStatement ps = (PreparedStatement) statementCache.get(sql);
+        if (ps == null) {
+            ps = connection.prepareStatement(sql);
+        }
+        ps.clearParameters();
+        return ps;
     }
 
     public Date getCreationTime() throws IOException {
@@ -103,18 +137,14 @@ class JdbcStore implements MessageStore {
     public void reset() throws IOException {
         cache.reset();
         try {
-            PreparedStatement deleteMessages = connection
-                    .prepareStatement("DELETE FROM messages WHERE beginstring=? and sendercompid=? "
-                            + "and targetcompid=? and session_qualifier=?");
+            PreparedStatement deleteMessages = getPreparedStatement(SQL_DELETE_MESSAGES);
             deleteMessages.setString(1, sessionID.getBeginString());
             deleteMessages.setString(2, sessionID.getSenderCompID());
             deleteMessages.setString(3, sessionID.getTargetCompID());
             deleteMessages.setString(4, sessionID.getSessionQualifier());
             deleteMessages.execute();
 
-            PreparedStatement updateTime = connection
-                    .prepareStatement("UPDATE sessions SET creation_time=?, incoming_seqnum=?, outgoing_seqnum=? "
-                            + "WHERE beginstring=? and sendercompid=? and targetcompid=? and session_qualifier=?");
+            PreparedStatement updateTime = getPreparedStatement(SQL_UPDATE_SESSION);
             updateTime.setTimestamp(1, new Timestamp(Calendar.getInstance(
                     TimeZone.getTimeZone("UTC")).getTimeInMillis()));
             updateTime.setInt(2, getNextTargetMsgSeqNum());
@@ -132,32 +162,36 @@ class JdbcStore implements MessageStore {
     }
 
     public void get(int startSequence, int endSequence, Collection messages) throws IOException {
+        ResultSet rs = null;
         try {
-            PreparedStatement query = connection
-                    .prepareStatement("SELECT message FROM messages WHERE  beginstring=? and sendercompid=? and "
-                            + "targetcompid=? and session_qualifier=? and msgseqnum>=? and msgseqnum<=? "
-                            + "ORDER BY msgseqnum");
+            PreparedStatement query = getPreparedStatement(SQL_GET_MESSAGES);
             query.setString(1, sessionID.getBeginString());
             query.setString(2, sessionID.getSenderCompID());
             query.setString(3, sessionID.getTargetCompID());
             query.setString(4, sessionID.getSessionQualifier());
             query.setInt(5, startSequence);
             query.setInt(6, endSequence);
-            ResultSet rs = query.executeQuery();
+            rs = query.executeQuery();
             while (rs.next()) {
                 Blob messageBlob = rs.getBlob(1);
                 messages.add(new String(messageBlob.getBytes(1, (int) messageBlob.length())));
             }
         } catch (SQLException e) {
             throw new IOException(e.getMessage());
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e1) {
+                    throw new IOException(e1.getMessage());
+                }
+            }
         }
     }
 
     public boolean set(int sequence, String message) throws IOException {
         try {
-            PreparedStatement insert = connection
-                    .prepareStatement("INSERT INTO messages (beginstring, sendercompid, targetcompid, "
-                            + "session_qualifier, msgseqnum, message) VALUES (?,?,?,?,?,?)");
+            PreparedStatement insert = getPreparedStatement(SQL_INSERT_MESSAGE);
             insert.setString(1, sessionID.getBeginString());
             insert.setString(2, sessionID.getSenderCompID());
             insert.setString(3, sessionID.getTargetCompID());
@@ -167,9 +201,7 @@ class JdbcStore implements MessageStore {
             insert.execute();
         } catch (SQLException ex) {
             try {
-                PreparedStatement update = connection
-                        .prepareStatement("UPDATE messages SET message=? WHERE beginstring=? and sendercompid=? "
-                                + "and targetcompid=? and session_qualifier=? and msgseqnum=?");
+                PreparedStatement update = getPreparedStatement(INSERT_UPDATE_MESSAGE);
                 update.setString(1, sessionID.getBeginString());
                 update.setString(2, sessionID.getSenderCompID());
                 update.setString(3, sessionID.getTargetCompID());
@@ -188,24 +220,23 @@ class JdbcStore implements MessageStore {
 
     public void setNextSenderMsgSeqNum(int next) throws IOException {
         cache.setNextSenderMsgSeqNum(next);
-        storeSequenceNumber(next, "outgoing_seqnum");
+        storeSequenceNumbers();
     }
 
     public void setNextTargetMsgSeqNum(int next) throws IOException {
         cache.setNextTargetMsgSeqNum(next);
-        storeSequenceNumber(next, "incoming_seqnum");
+        storeSequenceNumbers();
     }
 
-    private void storeSequenceNumber(int next, String column) throws IOException {
+    private void storeSequenceNumbers() throws IOException {
         try {
-            PreparedStatement update = connection.prepareStatement("UPDATE sessions SET " + column
-                    + "=? WHERE beginstring=? and sendercompid=? "
-                    + "and targetcompid=? and session_qualifier=?");
-            update.setInt(1, next);
-            update.setString(2, sessionID.getBeginString());
-            update.setString(3, sessionID.getSenderCompID());
-            update.setString(4, sessionID.getTargetCompID());
-            update.setString(5, sessionID.getSessionQualifier());
+            PreparedStatement update = getPreparedStatement(SQL_UPDATE_SEQNUMS);
+            update.setInt(1, cache.getNextTargetMsgSeqNum());
+            update.setInt(2, cache.getNextSenderMsgSeqNum());
+            update.setString(3, sessionID.getBeginString());
+            update.setString(4, sessionID.getSenderCompID());
+            update.setString(5, sessionID.getTargetCompID());
+            update.setString(6, sessionID.getSessionQualifier());
             update.execute();
         } catch (SQLException e) {
             throw new IOException(e.getMessage());
