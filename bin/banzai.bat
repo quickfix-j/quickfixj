6804e2c885835a3133db9b1c72e08268aa4dde2f
Steven Bate <steve@stevebate.net>
2006-03-24 18:35:25 +0000

    #176 Example scripts had wrong library references

--- bin/banzai.bat
+++ bin/banzai.bat
@@ -1,10 +1,8 @@
 @echo off
-set CLASSPATH=quickfixj.jar
 set CLASSPATH=%CLASSPATH%;output/ant/jars/quickfixj.jar
-set CLASSPATH=%CLASSPATH%;output/eclipse/classes
-set CLASSPATH=%CLASSPATH%;lib/tl-netty2-1.8.0.jar
-set CLASSPATH=%CLASSPATH%;lib/backport-util-concurrent-2.0.jar
-set CLASSPATH=%CLASSPATH%;lib/commons-logging-1.0.4.jar
+set CLASSPATH=%CLASSPATH%;lib/mina-0.8.2.jar
+set CLASSPATH=%CLASSPATH%;lib/backport-util-concurrent-2.1.jar
+set CLASSPATH=%CLASSPATH%;lib/slf4j-jdk14-1.0-rc5.jar
 
 cd ..
 java quickfix.examples.banzai.Banzai
--- bin/executor.sh
+++ bin/executor.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-CLASSPATH=quickfixj.jar
 CLASSPATH=$CLASSPATH:output/ant/jars/quickfixj.jar
 CLASSPATH=$CLASSPATH:output/eclipse/classes
-CLASSPATH=$CLASSPATH:lib/tl-netty2-1.8.0.jar
-CLASSPATH=$CLASSPATH:lib/backport-util-concurrent-2.0.jar
-CLASSPATH=$CLASSPATH:lib/commons-logging-1.0.4.jar
+CLASSPATH=$CLASSPATH:lib/mina-0.8.2.jar
+CLASSPATH=$CLASSPATH:lib/backport-util-concurrent-2.1.jar
+CLASSPATH=$CLASSPATH:lib/slf4j-jdk14-1.0-rc5.jar
 export CLASSPATH
 
 cd ..
