c66a74784b11336fe343b15678acc39e91353956
Steven Bate <steve@stevebate.net>
2006-05-20 11:09:07 +0000

    Updated classpaths for new JAR files

--- bin/executor.sh
+++ bin/executor.sh
@@ -2,9 +2,9 @@
 
 CLASSPATH=$CLASSPATH:output/ant/jars/quickfixj.jar
 CLASSPATH=$CLASSPATH:output/eclipse/classes
-CLASSPATH=$CLASSPATH:lib/mina-0.8.2.jar
+CLASSPATH=$CLASSPATH:lib/mina-core-0.93.jar
 CLASSPATH=$CLASSPATH:lib/backport-util-concurrent-2.1.jar
-CLASSPATH=$CLASSPATH:lib/slf4j-jdk14-1.0-rc5.jar
+CLASSPATH=$CLASSPATH:lib/slf4j-jdk14.jar
 export CLASSPATH
 
 cd ..
