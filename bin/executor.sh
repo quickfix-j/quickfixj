26d647bb552cb8a0aee57aebdcc9167e2ca18eef
Steven Bate <steve@stevebate.net>
2006-06-17 07:47:02 +0000

    Merge QFJ_RELEASE_1_0_x rev 422 -> trunk

--- bin/executor.sh
+++ bin/executor.sh
@@ -4,8 +4,7 @@ CLASSPATH=$CLASSPATH:output/ant/jars/quickfixj.jar
 CLASSPATH=$CLASSPATH:output/eclipse/classes
 CLASSPATH=$CLASSPATH:lib/mina-core-0.93.jar
 CLASSPATH=$CLASSPATH:lib/backport-util-concurrent-2.1.jar
-CLASSPATH=$CLASSPATH:lib/slf4j-jdk14.jar
-export CLASSPATH
+CLASSPATH=$CLASSPATH:lib/slf4j-jdk14-1.0.1.jar
 
 cd ..
-java quickfix.examples.executor.Executor
+java -classpath "$CLASSPATH" quickfix.examples.executor.Executor
