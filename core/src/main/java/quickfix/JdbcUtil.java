7d956a5a0126589ac671533091f62cb30e4f68fd
Joerg Thoennes <joerg.thoennes@macd.com>
2007-05-31 19:49:22 +0000

    QFJ-164: Add ability to set data source directly on log/store and remove JNDI property initialization. - Organized imports.

--- core/src/main/java/quickfix/JdbcUtil.java
+++ core/src/main/java/quickfix/JdbcUtil.java
@@ -23,7 +23,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Hashtable;
 import java.util.Map;
 
 import javax.naming.InitialContext;
