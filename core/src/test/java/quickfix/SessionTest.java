0c8f3bf9029309ead4a23b144103b25741e36bb1
Steven Bate <steve@stevebate.net>
2007-06-10 14:49:38 +0000

    QFJ-191 	 Add transactional behaviour to fromApp() callback Added test for sequence number "rollback" from callback exceptions.

--- core/src/test/java/quickfix/SessionTest.java
+++ core/src/test/java/quickfix/SessionTest.java
@@ -5,12 +5,17 @@ import java.lang.reflect.Field;
 import java.util.Date;
 
 import junit.framework.TestCase;
+import quickfix.field.Headline;
 import quickfix.field.HeartBtInt;
 import quickfix.field.MsgSeqNum;
+import quickfix.field.MsgType;
 import quickfix.field.SenderCompID;
 import quickfix.field.SendingTime;
 import quickfix.field.TargetCompID;
+import quickfix.field.TestReqID;
 import quickfix.fix44.Logon;
+import quickfix.fix44.News;
+import quickfix.fix44.TestRequest;
 
 /**
  * Note: most session tests are in the form of acceptance tests.
@@ -33,20 +38,7 @@ public class SessionTest extends TestCase {
         };
 
         Session session = setUpSession(application);
-
-        Field stateField = session.getClass().getDeclaredField("state");
-        stateField.setAccessible(true);
-        SessionState state = (SessionState) stateField.get(session);
-
-        assertEquals(false, state.isInitiator());
-        assertEquals(false, state.isLogonSent());
-        assertEquals(false, state.isLogonReceived());
-        assertEquals(false, state.isLogonAlreadySent());
-        assertEquals(false, state.isLogonSendNeeded());
-        assertEquals(false, state.isLogonTimedOut());
-        assertEquals(false, state.isLogoutSent());
-        assertEquals(false, state.isLogoutReceived());
-        assertEquals(false, state.isLogoutTimedOut());
+        SessionState state = getSessionState(session);
 
         assertEquals(1, state.getNextSenderMsgSeqNum());
         assertEquals(1, state.getNextTargetMsgSeqNum());
@@ -66,6 +58,109 @@ public class SessionTest extends TestCase {
         assertEquals(2, state.getNextTargetMsgSeqNum());
     }
 
+    public void testSequenceRollbackOnCallbackException() throws Exception {
+
+        // Create application that rejects all logons
+        Application application = new UnitTestApplication() {
+
+            public void fromApp(Message message, SessionID sessionId) throws FieldNotFound,
+                    IncorrectDataFormat, IncorrectTagValue, UnsupportedMessageType {
+                super.fromApp(message, sessionId);
+                throw new RuntimeException("TEST");
+            }
+
+            public void fromAdmin(Message message, SessionID sessionId) throws FieldNotFound,
+                    IncorrectDataFormat, IncorrectTagValue, RejectLogon {
+                super.fromAdmin(message, sessionId);
+                if (message.getHeader().getString(MsgType.FIELD).equals(MsgType.LOGON)) {
+                    return;
+                }
+                throw new RuntimeException("TEST");
+            }
+
+        };
+
+        Session session = setUpSession(application);
+        SessionState state = getSessionState(session);
+
+        logonTo(session);
+        assertEquals(2, state.getNextSenderMsgSeqNum());
+        assertEquals(2, state.getNextTargetMsgSeqNum());
+
+        processMessage(session, createAppMessage(2));
+
+        assertEquals(2, state.getNextSenderMsgSeqNum());
+        assertEquals(2, state.getNextTargetMsgSeqNum());
+
+        // To avoid resendRequest
+        state.setNextTargetMsgSeqNum(3);
+        
+        processMessage(session, createAdminMessage(3));
+
+        assertEquals(2, state.getNextSenderMsgSeqNum());
+        assertEquals(3, state.getNextTargetMsgSeqNum());
+}
+
+    private void processMessage(Session session, Message message) throws FieldNotFound, RejectLogon, IncorrectDataFormat, IncorrectTagValue, UnsupportedMessageType, IOException, InvalidMessage {
+        try {
+            session.next(message);
+        } catch (Throwable e) {
+            // This simulated the behavior of the QFJ connectors
+            // The will just discard a message with an error (without
+            // incrementing the sequence number).
+        }
+    }
+
+    private Message createAdminMessage(int sequence) {
+        TestRequest msg = new TestRequest(new TestReqID("ID"));
+        msg.getHeader().setString(SenderCompID.FIELD, "TARGET");
+        msg.getHeader().setString(TargetCompID.FIELD, "SENDER");
+        msg.getHeader().setInt(MsgSeqNum.FIELD, sequence);
+        msg.getHeader().setUtcTimeStamp(SendingTime.FIELD, new Date());
+        return msg;
+    }
+
+    public void testDontCatchErrorsFromCallback() throws Exception {
+
+        // Create application that rejects all logons
+        Application application = new UnitTestApplication() {
+
+            public void fromApp(Message message, SessionID sessionId) throws FieldNotFound,
+                    IncorrectDataFormat, IncorrectTagValue, UnsupportedMessageType {
+                super.fromApp(message, sessionId);
+                throw new Error("TEST");
+            }
+
+        };
+
+        Session session = setUpSession(application);
+        logonTo(session);
+
+        try {
+            session.next(createAppMessage(2));
+            fail("No error thrown");
+        } catch (Error e) {
+            assertEquals("TEST", e.getMessage());
+        }
+    }
+
+    private News createAppMessage(int sequence) {
+        News news = new News(new Headline("Headline"));
+        news.getHeader().setString(SenderCompID.FIELD, "TARGET");
+        news.getHeader().setString(TargetCompID.FIELD, "SENDER");
+        news.getHeader().setInt(MsgSeqNum.FIELD, sequence);
+        news.getHeader().setUtcTimeStamp(SendingTime.FIELD, new Date());
+        return news;
+    }
+
+    private SessionState getSessionState(Session session) throws NoSuchFieldException,
+            IllegalAccessException {
+        Field stateField = session.getClass().getDeclaredField("state");
+        stateField.setAccessible(true);
+        SessionState state = (SessionState) stateField.get(session);
+        return state;
+    }
+
     /** Veifies that the session has been registered before the logger tries accessing it
      * Use case:
      *  JdbcLogger not setup correctly, barfs during Session creation, tries to log and
@@ -76,22 +171,35 @@ public class SessionTest extends TestCase {
         settings.setString(Session.SETTING_USE_DATA_DICTIONARY, "N");
         JdbcTestSupport.setHypersonicSettings(settings);
         // do not initialize the SQL tables so that the JdbcLog will fail
-        SessionID sessionID = new SessionID("FIX.4.2", "SENDER-sessionRegister", "TARGET-sessionRegister");
+        SessionID sessionID = new SessionID("FIX.4.2", "SENDER-sessionRegister",
+                "TARGET-sessionRegister");
         settings.setString(sessionID, "ConnectionType", "acceptor");
-        DefaultSessionFactory factory = new DefaultSessionFactory(new UnitTestApplication(), new MemoryStoreFactory(),
-                                                                    new JdbcLogFactory(settings));
+        DefaultSessionFactory factory = new DefaultSessionFactory(new UnitTestApplication(),
+                new MemoryStoreFactory(), new JdbcLogFactory(settings));
         try {
             Session session = factory.create(sessionID, settings);
             assertNotNull(session);
-        } catch(NullPointerException nex) {
-            fail("Session not registering correctly so JdbcLog fails while printing an error: "+nex.getMessage());
+        } catch (NullPointerException nex) {
+            fail("Session not registering correctly so JdbcLog fails while printing an error: "
+                    + nex.getMessage());
         }
     }
 
-    private Session setUpSession(Application application) {
+    private Session setUpSession(Application application) throws NoSuchFieldException,
+            IllegalAccessException {
         SessionID sessionID = new SessionID(FixVersions.BEGINSTRING_FIX44, "SENDER", "TARGET");
         Session session = SessionFactoryTestSupport.createSession(sessionID, application, false);
         session.setResponder(new UnitTestResponder());
+        SessionState state = getSessionState(session);
+        assertEquals(false, state.isInitiator());
+        assertEquals(false, state.isLogonSent());
+        assertEquals(false, state.isLogonReceived());
+        assertEquals(false, state.isLogonAlreadySent());
+        assertEquals(false, state.isLogonSendNeeded());
+        assertEquals(false, state.isLogonTimedOut());
+        assertEquals(false, state.isLogoutSent());
+        assertEquals(false, state.isLogoutReceived());
+        assertEquals(false, state.isLogoutTimedOut());
         return session;
     }
 
