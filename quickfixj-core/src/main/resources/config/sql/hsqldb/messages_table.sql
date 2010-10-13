dee7a3de3b23df19f1a8b2f34089ca74277651b6
Claudio Bantaloukas <rockdreamer@gmail.com>
2010-10-13 20:54:33 +0000

    add hsqldb specific sql files

---
+++ quickfixj-core/src/main/resources/config/sql/hsqldb/messages_table.sql
@@ -0,0 +1,17 @@
+USE quickfix;
+DROP TABLE messages IF EXISTS
+CREATE TABLE messages (
+  beginstring CHAR(8) NOT NULL,
+  sendercompid VARCHAR(64) NOT NULL,
+  sendersubid VARCHAR(64) NOT NULL,
+  senderlocid VARCHAR(64) NOT NULL,
+  targetcompid VARCHAR(64) NOT NULL,
+  targetsubid VARCHAR(64) NOT NULL,
+  targetlocid VARCHAR(64) NOT NULL,
+  session_qualifier VARCHAR(64) NOT NULL,
+  msgseqnum INT NOT NULL, 
+  message TEXT NOT NULL,
+  PRIMARY KEY (beginstring, sendercompid, sendersubid, senderlocid, 
+  				targetcompid, targetsubid, targetlocid, session_qualifier,
+  				msgseqnum)
+);
