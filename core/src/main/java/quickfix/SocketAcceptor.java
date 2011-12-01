4de3dd229eaba02a93eaa60b0b3b2e760628d33b
Christoph John <christoph.john@macd.com>
2011-12-01 13:51:38 +0000

    QFJ-563: SocketAcceptor initialization is not properly synchronized  - Introduced private final lock Object.

--- core/src/main/java/quickfix/SocketAcceptor.java
+++ core/src/main/java/quickfix/SocketAcceptor.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
- * Copyright (c) quickfixengine.org  All rights reserved. 
- * 
- * This file is part of the QuickFIX FIX Engine 
- * 
- * This file may be distributed under the terms of the quickfixengine.org 
- * license as defined by quickfixengine.org and appearing in the file 
- * LICENSE included in the packaging of this file. 
- * 
- * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING 
- * THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A 
- * PARTICULAR PURPOSE. 
- * 
- * See http://www.quickfixengine.org/LICENSE for licensing information. 
- * 
- * Contact ask@quickfixengine.org if any conditions of this licensing 
+ * Copyright (c) quickfixengine.org  All rights reserved.
+ *
+ * This file is part of the QuickFIX FIX Engine
+ *
+ * This file may be distributed under the terms of the quickfixengine.org
+ * license as defined by quickfixengine.org and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+ * THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ *
+ * See http://www.quickfixengine.org/LICENSE for licensing information.
+ *
+ * Contact ask@quickfixengine.org if any conditions of this licensing
  * are not clear to you.
  ******************************************************************************/
 
@@ -30,6 +30,7 @@ import quickfix.mina.acceptor.AbstractSocketAcceptor;
  */
 public class SocketAcceptor extends AbstractSocketAcceptor {
     private Boolean isStarted = Boolean.FALSE;
+    private final Object lock = new Object();
 
     public SocketAcceptor(Application application, MessageStoreFactory messageStoreFactory,
             SessionSettings settings, LogFactory logFactory, MessageFactory messageFactory)
@@ -48,26 +49,26 @@ public class SocketAcceptor extends AbstractSocketAcceptor {
 
     private SingleThreadedEventHandlingStrategy eventHandlingStrategy =
         new SingleThreadedEventHandlingStrategy(this);
-    
+
     public void block() throws ConfigError, RuntimeError {
         initialize();
         eventHandlingStrategy.block();
     }
-    
+
     public void start() throws ConfigError, RuntimeError {
         initialize();
         eventHandlingStrategy.blockInThread();
     }
 
     private void initialize() throws ConfigError {
-        synchronized (isStarted) {
+        synchronized (lock) {
             if (isStarted.equals(Boolean.FALSE)) {
                 startAcceptingConnections();
             }
             isStarted = Boolean.TRUE;
         }
     }
-    
+
     public void stop() {
         stop(false);
     }
