dbc526c9bf315364d80adafe8185e04281648451
Amichai Rothman <amichai2@amichais.net>
2014-05-22 02:20:22 +0300

    standardize license header on GenerateMojo (with permission from author)

--- quickfixj-codegenerator/src/main/java/org/quickfixj/codegenerator/GenerateMojo.java
+++ quickfixj-codegenerator/src/main/java/org/quickfixj/codegenerator/GenerateMojo.java
@@ -1,19 +1,22 @@
-/*
- * Copyright 2005 The Codehaus.
- * Copyright 2010 Claudio Bantaloukas
+/*******************************************************************************
+ * Copyright (c) quickfixengine.org  All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This file is part of the QuickFIX FIX Engine
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * This file may be distributed under the terms of the quickfixengine.org
+ * license as defined by quickfixengine.org and appearing in the file
+ * LICENSE included in the packaging of this file.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+ * THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ *
+ * See http://www.quickfixengine.org/LICENSE for licensing information.
+ *
+ * Contact ask@quickfixengine.org if any conditions of this licensing
+ * are not clear to you.
+ ******************************************************************************/
+
 package org.quickfixj.codegenerator;
 
 import java.io.File;
