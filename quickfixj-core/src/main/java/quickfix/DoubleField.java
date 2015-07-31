b0d10c6cde03067a0b8c47cd72f2567aaed570c3
charlesbr1 <charlesbr1@users.noreply.github.com>
2015-07-31 10:23:05 -0500

    Allocations optimization
    Use the DoubleConverter to convert value to a string.
    Avoid creation of a Double instance when using a scalar double.

--- quickfixj-core/src/main/java/quickfix/DoubleField.java
+++ quickfixj-core/src/main/java/quickfix/DoubleField.java
@@ -19,30 +19,37 @@
 
 package quickfix;
 
+import quickfix.field.converter.DoubleConverter;
+
 /**
  * A double-values message field.
  */
 public class DoubleField extends Field<Double> {
 
+    private static final Double ZERO = 0d;
+    private double value = 0d;
     private int padding = 0;
 
     public DoubleField(int field) {
-        super(field, 0d);
+        super(field, ZERO);
     }
 
     public DoubleField(int field, Double data) {
         super(field, data);
         checkForValidDouble(data);
+        value = data;
     }
 
     public DoubleField(int field, double data) {
-        super(field, data);
+        super(field, ZERO);
         checkForValidDouble(data);
+        value = data;
     }
 
     public DoubleField(int field, double data, int padding) {
-        super(field, data);
+        super(field, ZERO);
         checkForValidDouble(data);
+        value = data;
         this.padding = padding;
     }
 
@@ -53,11 +60,26 @@ public class DoubleField extends Field<Double> {
 
     public void setValue(double value) {
         checkForValidDouble(value);
-        setObject(value);
+        this.value = value;
     }
 
     public double getValue() {
-        return getObject();
+        return value;
+    }
+
+    @Override
+    protected void setObject(Double object) {
+        super.setObject(object);
+        value = object;
+    }
+
+    @Override
+    public Double getObject() {
+        Double val = super.getObject();
+        if(null != val && value == val)
+            return val;
+        super.setObject(value);
+        return super.getObject();
     }
 
     public int getPadding() {
@@ -65,17 +87,52 @@ public class DoubleField extends Field<Double> {
     }
 
     public boolean valueEquals(Double value) {
-        return getObject().equals(value);
+        return null != value && valueEquals(value.doubleValue());
     }
 
     public boolean valueEquals(double value) {
-        return getObject().equals(value);
+        return this.value == value;
     }
 
     // QFJ-808: NaN or infinity values cannot be transmitted via FIX in a DoubleField
-    private void checkForValidDouble(Double value) throws NumberFormatException {
+    private void checkForValidDouble(double value) throws NumberFormatException {
         if (Double.isInfinite(value) || Double.isNaN(value)) {
             throw new NumberFormatException("Tried to set NaN or infinite value.");
         }
+    }
+
+    @Override
+    void toString(StringBuilder buffer) {
+        if (null != super.data) {
+            buffer.append(super.data);
+            return;
+        }
+        buffer.append(NumbersCache.get(getTag())).append('=');
+        double val = value;
+        if(val < 0d) {
+            buffer.append('-');
+            val = Math.abs(val);
+        }
+        String str = NumbersCache.get(val);
+        if(null != str)
+            buffer.append(str);
+        else
+            buffer.append(val);
+    }
+
+    @Override
+    protected String objectAsString() {
+        return DoubleConverter.convert(value);
+    }
+
+    @Override
+    protected void objectAsString(StringBuilder stringBuilder) {
+        stringBuilder.append(value);
+    }
+
+    @Override
+    public int hashCode() {
+        long bits = Double.doubleToLongBits(value);
+        return (int)(bits ^ (bits >>> 32));
     }
 }
