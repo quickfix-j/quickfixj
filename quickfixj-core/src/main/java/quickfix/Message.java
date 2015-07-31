8a1cfb78666a5f601c311931410781163a336058
charlesbr1 <charlesbr1@users.noreply.github.com>
2015-07-31 12:41:39 -0500

    Allocations optimization
    Put the string builder used in toString method in a thread local in order to reuse it.
    Calculate the message length & checksum after the message is built,  this avoid to compute the message multiple time and to iterate over each field.
    If the charset used to send message is not string equivalent to the jvm one, this optimization is not active.

--- quickfixj-core/src/main/java/quickfix/Message.java
+++ quickfixj-core/src/main/java/quickfix/Message.java
@@ -129,6 +129,21 @@ public class Message extends FieldMap {
         return message;
     }
 
+    private static final class Context {
+        private final BodyLength bodyLength = new BodyLength(100);
+        private final CheckSum checkSum = new CheckSum("000");
+        private final StringBuilder stringBuilder = new StringBuilder(1024);
+    }
+
+    private static final ThreadLocal<Context> stringContexts = new ThreadLocal<Context>() {
+        @Override
+        protected Context initialValue() {
+            return new Context();
+        }
+    };
+
+    private static final boolean isStringEquivalent = CharsetSupport.isStringEquivalent(CharsetSupport.getCharsetInstance());
+
     /**
      * Do not call this method concurrently while modifying the contents of the message.
      * This is likely to produce unexpected results or will fail with a ConcurrentModificationException
@@ -136,16 +151,118 @@ public class Message extends FieldMap {
      */
     @Override
     public String toString() {
-        final int bodyLength = bodyLength();
-        header.setInt(BodyLength.FIELD, bodyLength);
-        trailer.setString(CheckSum.FIELD, checksum());
+        Context context = stringContexts.get();
+        if(isStringEquivalent) { // length & checksum can easily be calculated after message is built
+            header.setField(context.bodyLength);
+            trailer.setField(context.checkSum);
+        } else {
+            header.setInt(BodyLength.FIELD, bodyLength());
+            trailer.setString(CheckSum.FIELD, checksum());
+        }
+        StringBuilder stringBuilder = context.stringBuilder;
+        try {
+            header.calculateString(stringBuilder, null, null);
+            calculateString(stringBuilder, null, null);
+            trailer.calculateString(stringBuilder, null, null);
+            if(isStringEquivalent) {
+                setBodyLength(stringBuilder);
+                setChecksum(stringBuilder);
+            }
+            return stringBuilder.toString();
+        } finally {
+            stringBuilder.setLength(0);
+        }
+    }
 
-        final StringBuilder sb = new StringBuilder(bodyLength);
-        header.calculateString(sb, null, null);
-        calculateString(sb, null, null);
-        trailer.calculateString(sb, null, null);
+    private static final String beginStringField = String.valueOf(BeginString.FIELD);
+    private static final String separatorFix = String.valueOf('\001');
+    private static final String bodyLengthField = separatorFix + String.valueOf(BodyLength.FIELD) + '=';
+    private static final String checkSumField = separatorFix + String.valueOf(CheckSum.FIELD) + '=';
 
-        return sb.toString();
+    private static void setBodyLength(StringBuilder stringBuilder) {
+        int index = indexOf(stringBuilder, beginStringField, 0);
+        if(index != 0)
+            throw new IllegalArgumentException("Malformed FIX message");
+        int bodyLengthIndex = getIndex(stringBuilder, bodyLengthField, 0);
+        index = getIndex(stringBuilder, separatorFix, bodyLengthIndex + 1);
+        int checkSumIndex = lastIndexOf(stringBuilder, checkSumField);
+        if(checkSumIndex < 0)
+            throw new IllegalArgumentException("Malformed FIX message");
+        int length = checkSumIndex - index;
+        bodyLengthIndex += bodyLengthField.length();
+        stringBuilder.replace(bodyLengthIndex, bodyLengthIndex + 3, NumbersCache.get(length));
+    }
+
+    private static void setChecksum(StringBuilder stringBuilder) {
+        int checkSumIndex = lastIndexOf(stringBuilder, checkSumField);
+        int checkSum = 0;
+        for(int i = checkSumIndex; i-- != 0;)
+            checkSum += stringBuilder.charAt(i);
+        String checkSumValue = NumbersCache.get((checkSum + 1) % 256);
+        checkSumIndex += checkSumField.length();
+        stringBuilder.replace(checkSumIndex + (3 - checkSumValue.length()), checkSumIndex + 3, checkSumValue);
+    }
+
+    private static int getIndex(StringBuilder stringBuilder, String value, int fromIndex) {
+        int index = indexOf(stringBuilder, value, fromIndex);
+        if(index < 0)
+            throw new IllegalArgumentException("Malformed FIX message");
+        return index;
+    }
+
+    // return index of a string in a stringbuilder without performing allocations
+    private static int indexOf(StringBuilder source, String target, int fromIndex) {
+        if (fromIndex >= source.length())
+            return (target.length() == 0 ? source.length() : -1);
+        if (fromIndex < 0)
+            fromIndex = 0;
+        if (target.length() == 0)
+            return fromIndex;
+        char first = target.charAt(0);
+        int max = source.length() - target.length();
+        for (int i = fromIndex; i <= max; i++) {
+            if (source.charAt(i) != first)
+                while (++i <= max && source.charAt(i) != first);
+            if (i <= max) {
+                int j = i + 1;
+                int end = j + target.length() - 1;
+                for (int k = 1; j < end && source.charAt(j)
+                                           == target.charAt(k); j++, k++);
+                if (j == end)
+                    return i;
+            }
+        }
+        return -1;
+    }
+
+    // return last index of a string in a stringbuilder without performing allocations
+    static int lastIndexOf(StringBuilder source, String target) {
+        int rightIndex = source.length() - target.length();
+        int fromIndex = source.length();
+        if (fromIndex > rightIndex)
+            fromIndex = rightIndex;
+        if (target.length() == 0)
+            return fromIndex;
+        int strLastIndex = target.length() - 1;
+        char strLastChar = target.charAt(strLastIndex);
+        int min = target.length() - 1;
+        int i = min + fromIndex;
+        startSearchForLastChar:
+        while (true) {
+            while (i >= min && source.charAt(i) != strLastChar)
+                i--;
+            if (i < min)
+                return -1;
+            int j = i - 1;
+            int start = j - (target.length() - 1);
+            int k = strLastIndex - 1;
+            while (j > start)
+                if (source.charAt(j--) != target.charAt(k--)) {
+                    i--;
+                    continue startSearchForLastChar;
+                }
+            return start + 1;
+        }
     }
 
     public int bodyLength() {
