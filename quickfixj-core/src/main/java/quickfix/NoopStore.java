fe683ee82f67b92b85c216fe3a3521af521c0c35
Christoph John <christoph.john@macd.com>
2016-12-12 13:46:13 +0100

    Merge pull request #87 from guidomedina/noop-message-store-implementation
    Noop message store implementation
    (cherry picked from commit 2a77d99ad3c88d0e472cb7ad7bd287f7bf80c6c1)

---
+++ quickfixj-core/src/main/java/quickfix/NoopStore.java
@@ -0,0 +1,95 @@
+/*
+ ******************************************************************************
+ * Copyright (c) quickfixengine.org  All rights reserved.
+ *
+ * This file is part of the QuickFIX FIX Engine
+ *
+ * This file may be distributed under the terms of the quickfixengine.org
+ * license as defined by quickfixengine.org and appearing in the file
+ * LICENSE included in the packaging of this file.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+ * THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ *
+ * See http://www.quickfixengine.org/LICENSE for licensing information.
+ *
+ * Contact ask@quickfixengine.org if any conditions of this licensing
+ * are not clear to you.
+ ******************************************************************************/
+
+package quickfix;
+
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.Date;
+
+/**
+ * No-op message store implementation.
+ *
+ * @see quickfix.MemoryStoreFactory
+ */
+public class NoopStore implements MessageStore {
+
+    private final SessionID sessionID;
+
+    private Date creationTime = new Date();
+    private int nextSenderMsgSeqNum = 1;
+    private int nextTargetMsgSeqNum = 1;
+
+    public NoopStore(SessionID sessionID) {
+        this.sessionID = sessionID;
+    }
+
+    public void get(int startSequence, int endSequence, Collection<String> messages) {
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
+    }
+
+    public int getNextSenderMsgSeqNum() {
+        return nextSenderMsgSeqNum;
+    }
+
+    public int getNextTargetMsgSeqNum() {
+        return nextTargetMsgSeqNum;
+    }
+
+    public void incrNextSenderMsgSeqNum() {
+        nextSenderMsgSeqNum++;
+    }
+
+    public void incrNextTargetMsgSeqNum() {
+        nextTargetMsgSeqNum++;
+    }
+
+    public void reset() {
+        creationTime = new Date();
+        nextSenderMsgSeqNum = 1;
+        nextTargetMsgSeqNum = 1;
+    }
+
+    public boolean set(int sequence, String message) {
+        return true;
+    }
+
+    public void setNextSenderMsgSeqNum(int next) {
+        nextSenderMsgSeqNum = next;
+    }
+
+    public void setNextTargetMsgSeqNum(int next) {
+        nextTargetMsgSeqNum = next;
+    }
+
+    public void refresh() {
+        final String text = "No-op store does not support refresh!";
+        if (sessionID != null) {
+            final Session session = Session.lookupSession(sessionID);
+            session.getLog().onErrorEvent("ERROR: " + text);
+        } else {
+            LoggerFactory.getLogger(NoopStore.class).error(text);
+        }
+    }
+}
