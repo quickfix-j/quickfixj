068623fb168bdd1805b6a13acd78ecfea345261a
Christoph John <christoph.john@macd.com>
2017-12-08 15:32:59 +0100

    corrected short-cut logic

--- quickfixj-core/src/test/java/quickfix/mina/ssl/SSLCertificateTest.java
+++ quickfixj-core/src/test/java/quickfix/mina/ssl/SSLCertificateTest.java
@@ -650,7 +650,7 @@ public class SSLCertificateTest {
             try {
                 X509Certificate[] peerCertificateChain = sslSession.getPeerCertificateChain();
 
-                if (peerCertificateChain != null & peerCertificateChain.length > 0) {
+                if (peerCertificateChain != null && peerCertificateChain.length > 0) {
                     throw new AssertionError("Certificate was authenticated");
                 }
             } catch (SSLPeerUnverifiedException e) {
