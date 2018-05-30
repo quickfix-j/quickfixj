4d9591d764479c006dc4177778dcf53a44a242eb
chrjohn <christoph.john@macd.com>
2018-05-30 13:55:18 +0200

    Added debug logging when sending/receiving message in AcceptanceTests.

--- quickfixj-core/src/test/java/quickfix/test/acceptance/TestConnection.java
+++ quickfixj-core/src/test/java/quickfix/test/acceptance/TestConnection.java
@@ -89,7 +89,6 @@ public class TestConnection {
             SessionConnector.closeManagedSessionsAndDispose(connector, true, log);
         }
 
-        System.out.println("quickfix.test.acceptance.TestConnection.connect() 1");
         SocketAddress address;
         if (transportType == ProtocolFactory.SOCKET) {
             connector = new NioSocketConnector();
@@ -102,16 +101,13 @@ public class TestConnection {
         }
         connectors.put(Integer.toString(clientId), connector);
 
-        System.out.println("quickfix.test.acceptance.TestConnection.connect() 2");
         TestIoHandler testIoHandler = new TestIoHandler();
         synchronized (ioHandlers) {
             ioHandlers.put(clientId, testIoHandler);
             connector.setHandler(testIoHandler);
             ConnectFuture future = connector.connect(address);
-            System.out.println("quickfix.test.acceptance.TestConnection.connect() 3");
             future.awaitUninterruptibly(5000L);
             Assert.assertTrue("connection to server failed", future.isConnected());
-            System.out.println("quickfix.test.acceptance.TestConnection.connect() 4");
         }
     }
 
@@ -140,6 +136,7 @@ public class TestConnection {
         }
 
         public void messageReceived(IoSession session, Object message) throws Exception {
+            System.out.println("XXXXXXXXX quickfix.test.acceptance.TestConnection.TestIoHandler.messageReceived() " + message);
             messages.add(message);
         }
 
