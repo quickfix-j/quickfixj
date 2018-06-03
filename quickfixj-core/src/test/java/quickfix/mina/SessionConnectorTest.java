aa88bc2de183a4aad352860c2cafcb78e5891f85
Christoph John <christoph.john@macd.com>
2018-06-03 22:45:04 +0200

    re-add import which was accidentally removed

--- quickfixj-core/src/test/java/quickfix/mina/SessionConnectorTest.java
+++ quickfixj-core/src/test/java/quickfix/mina/SessionConnectorTest.java
@@ -46,6 +46,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
