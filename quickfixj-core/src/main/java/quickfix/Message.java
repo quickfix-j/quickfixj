03c77184bdd0103167d3b2f484dca10ecd1155cd
Christoph John <christoph.john@macd.com>
2021-04-25 01:36:51 +0200

    corrected typo (#389)

--- quickfixj-core/src/main/java/quickfix/Message.java
+++ quickfixj-core/src/main/java/quickfix/Message.java
@@ -306,7 +306,7 @@ public class Message extends FieldMap {
 
     /**
      * Converts the message into a simple XML format. This format is
-     * probably not sufficient for production use, but it more intended
+     * probably not sufficient for production use, but is more intended
      * for diagnostics and debugging. THIS IS NOT FIXML.
      *
      * To get names instead of tag number, use toXML(DataDictionary)
@@ -321,7 +321,7 @@ public class Message extends FieldMap {
 
     /**
      * Converts the message into a simple XML format. This format is
-     * probably not sufficient for production use, but it more intended
+     * probably not sufficient for production use, but is more intended
      * for diagnostics and debugging. THIS IS NOT FIXML.
      *
      * @param dataDictionary
