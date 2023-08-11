ca96dde2683e39ce5095fde19e05d5ddcdcc0184
Christoph John <christoph.john@macd.com>
2023-08-11 12:01:31 +0200

    Revert "Correct `Resource not found` in `FileUtilTest` (#654)"
    This reverts commit 962db7946644ea434730b48c5932a2559b05a89b.

--- quickfixj-core/src/main/java/quickfix/FileUtil.java
+++ quickfixj-core/src/main/java/quickfix/FileUtil.java
@@ -24,10 +24,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
-
 
 public class FileUtil {
     public static String fileAppendPath(String pathPrefix, String pathSuffix) {
@@ -144,18 +141,7 @@ public class FileUtil {
                 break;
             case URL:
                 try {
-                    URL url = new URL(name);
-                    URLConnection urlConnection = url.openConnection();
-                    if (urlConnection instanceof HttpURLConnection) {
-                        HttpURLConnection httpURLConnection = (HttpURLConnection)urlConnection;
-                        httpURLConnection.setRequestProperty("User-Agent", "Java-QuickFIXJ-FileUtil");
-                        httpURLConnection.connect();
-                        in = httpURLConnection.getInputStream();
-                    } else {
-                        if (urlConnection != null) {
-                            in = urlConnection.getInputStream();
-                        }
-                    }
+                    in = new URL(name).openStream();
                 } catch (IOException e) {
                     // ignore
                 }
