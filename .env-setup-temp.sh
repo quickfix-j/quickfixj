8ea4efa3f9a0a58eea051e7a23aa4735bbcb25ca
Christoph John <christoph.john@macd.com>
2025-11-25 15:26:32 +0000

    removed unneeded files

--- .env-setup-temp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-cd "$(dirname "$0")"
-./mvnw clean verify -Dmaven.test.skip=false -B
--- .env-setup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd /workspaces/quickfixj
-./mvnw -B clean verify
